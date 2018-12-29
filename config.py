612076c0158779806ac6d387d6788432840817e2
Davarice <jsdreim@gmail.com>
2018-12-29 01:01:21 -0500

    Prefix added

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+prefix = "+"
+
 class Msg:
     preface = "The Auction will run for {auction-length} minutes. Focus on chat and not any 'live' video since there might be a slight delay. I will confirm bids in chat. I will do a final 5,4,3,2,1,0 countdown after which the auction is over. The person with the highest bid will be declared the winner and they will have to tip that amount to claim their prize. Bidding starts at ${bid-initial}."
     start = "The Auction begins NOW."
